af0c9c3f3de48038e49041b354b8c43bb3741d01
MandoMG <mando.mg.91@gmail.com>
2021-10-03 23:59:34 -0500

    Added balance and mock list to dashboard screen on mobile

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -11,101 +11,17 @@
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { useTestHook } from "mandomg-expensetracker-common/src/hooks";
-import TestReactNative from "mandomg-expensetracker-common/src/components/testReactNative";
-
-const Section: React.FC<{
-  title: string;
-}> = ({ children, title }) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import Home from './src/screens/Home';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const { memoWelcomeString } = useTestHook();
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            <Text>{memoWelcomeString}</Text>
-          </Section>
-          <Section title="Shared">
-            <TestReactNative />
-          </Section>
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <StatusBar />
+      <Home />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ packages/mobile/src/screens/components/Header.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { ImageBackground, Text, useWindowDimensions, View } from 'react-native';
+import CategoryList from './CategoryList';
+
+const HomeHeader = () => {
+   const { height } = useWindowDimensions();
+   return (
+      <View style={{ width: '100%', height: height * .65 }}>
+         <ImageBackground
+            source={require('../../assets/header-background.png')}
+            resizeMode="cover"
+            imageStyle={{ borderBottomRightRadius: 25, borderBottomLeftRadius: 25 }}
+            style={{
+               flex: 1,
+            }}
+         >
+            <View style={{
+               paddingTop: 20,
+               alignItems: 'center',
+               justifyContent: 'center',
+            }}>
+               <Text style={{ color: 'white', fontSize: 18 }}> Dashboard </Text>
+            </View>
+            <View style={{
+               paddingTop: 10,
+               paddingLeft: 20,
+               alignItems: 'flex-start',
+               justifyContent: 'flex-start',
+            }}>
+               <Text style={{ color: 'white', fontSize: 14 }}> Current Balance </Text>
+               <Text style={{ color: 'white', fontSize: 28, fontWeight: 'bold' }}> $1420.69 </Text>
+            </View>
+            <View style={{
+               paddingTop: 10,
+               paddingHorizontal: 20,
+               alignItems: 'flex-start',
+               justifyContent: 'flex-start',
+               flexDirection: 'row'
+            }}>
+               <View style={{
+                  flex: 1,
+                  alignItems: 'flex-start',
+                  justifyContent: 'flex-start',
+               }}>
+                  <Text style={{ color: 'white', fontSize: 14 }}> Income </Text>
+                  <Text style={{ color: 'white', fontSize: 28, fontWeight: 'bold' }}> $2000.00 </Text>
+               </View>
+               <View style={{
+                  flex: 1,
+                  alignItems: 'flex-end',
+                  justifyContent: 'flex-end',
+               }}>
+                  <Text style={{ color: 'white', fontSize: 14 }}> Income </Text>
+                  <Text style={{ color: 'white', fontSize: 28, fontWeight: 'bold' }}> $579.31 </Text>
+               </View>
+            </View>
+            <CategoryList />
+         </ImageBackground>
+      </View>
+   )
+}
+
+export default HomeHeader;
