926d8b32582c7aea5b566833c706580a4c8c7b60
MandoMG <mando.mg.91@gmail.com>
2021-10-15 09:33:08 -0500

    Added image to navbar

--- packages/web/src/components/NavBar.tsx
+++ packages/web/src/components/NavBar.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from 'react-native-web';
+import { Image, Text, View } from 'react-native-web';
+import profilePicture from '../assets/test_profile_picture.jpg';
 
 const NavBar = () => {
    return (
-      <View style={{ marginVertical: 20, marginHorizontal: 25 }}>
-         <Text>Expense Tracker (Spese)</Text>
+      <View style={{ flex: 1, marginVertical: 20, marginHorizontal: 25, flexDirection: 'row' }}>
+         <View style={{ flex: 1, alignContent: 'flex-start' }}>
+            <Text>Expense Tracker (Spese)</Text>
+         </View>
+         <View style={{ flex: 1, alignItems: 'flex-end' }}>
+            <Image source={{ uri: profilePicture }} style={{ height: 32, width: 32, borderRadius: 10 }} />
+         </View>
       </View>
    )
 }
