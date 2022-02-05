2f8e32e7b2f3510c062655a16a0615024fe0c3e5
MandoMG <mando.mg.91@gmail.com>
2022-02-04 23:38:27 -0600

    Added redesign to balance summary component

--- packages/mobile/src/screens/Home/Home.tsx
+++ packages/mobile/src/screens/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import TextUtil from 'mandomg-expensetracker-common/src/util/TextUtil';
+import { View } from 'react-native';
+import BalanceSummaryComponent from '../../components/balance/BalanceSummary';
 import ScreenHeaderComponent from '../../components/headers/ScreenHeader';
 import BudgetSummary from './components/BudgetSummary';
 
@@ -12,63 +12,7 @@ const Home = () => {
    return (
       <View>
          <ScreenHeaderComponent title="Dashboard" />
-         <View>
-            <View style={[{
-               backgroundColor: '#323246',
-               marginTop: 20,
-               marginHorizontal: 20,
-               borderRadius: 25,
-               elevation: 50,
-            }]}>
-               <View style={{
-                  paddingVertical: 15,
-                  paddingLeft: 20,
-                  alignItems: 'flex-start',
-                  justifyContent: 'flex-start',
-               }}>
-                  <Text style={{ color: 'white', fontSize: 14 }}> Current Balance </Text>
-                  <Text style={{ color: 'white', fontSize: 36, fontWeight: 'bold' }}>{TextUtil.formatCurrency(currentBalance, 0)}</Text>
-               </View>
-            </View>
-            <View style={{ flexDirection: 'row', marginHorizontal: 20 }}>
-               <View style={[{
-                  flex: 1,
-                  backgroundColor: '#8132ff',
-                  marginRight: 10,
-                  marginTop: 20,
-                  borderRadius: 25,
-                  elevation: 20,
-               }]}>
-                  <View style={{
-                     paddingVertical: 15,
-                     paddingHorizontal: 15,
-                     alignItems: 'flex-start',
-                     justifyContent: 'flex-start',
-                  }}>
-                     <Text style={{ color: 'white', fontSize: 14 }}> Income </Text>
-                     <Text style={{ color: 'white', fontSize: 28, fontWeight: 'bold' }}>{TextUtil.formatCurrency(income, 0)}</Text>
-                  </View>
-               </View>
-               <View style={[{
-                  flex: 1,
-                  backgroundColor: '#ff6937',
-                  marginLeft: 10,
-                  marginTop: 20,
-                  borderRadius: 25,
-                  elevation: 20,
-               }]}>
-                  <View style={{
-                     paddingVertical: 15,
-                     paddingHorizontal: 15,
-                     alignItems: 'flex-start',
-                     justifyContent: 'flex-start',
-                  }}>
-                     <Text style={{ color: 'white', fontSize: 14 }}> Expenses </Text>
-                     <Text style={{ color: 'white', fontSize: 28, fontWeight: 'bold' }}>{TextUtil.formatCurrency(expenses, 0)}</Text>
-                  </View>
-               </View>
-            </View>
-         </View>
+         <BalanceSummaryComponent currentBalance={currentBalance} income={income} expenses={expenses} />
          <View style={{
             height: '58%',
          }}>
