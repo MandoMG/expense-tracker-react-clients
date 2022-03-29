c2cbb474f5d1414874f1072d5dfe9e09c42447ca
MandoMG <mando.mg.91@gmail.com>
2022-03-28 20:52:54 -0500

    Fixed height in dashboard flatlist

--- packages/mobile/src/screens/Home/Home.tsx
+++ packages/mobile/src/screens/Home/Home.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { View } from 'react-native';
+import { useWindowDimensions, View } from 'react-native';
 import BalanceSummaryComponent from '../../components/balance/BalanceSummary';
 import ScreenHeaderComponent from '../../components/headers/ScreenHeader';
 import BudgetSummary from './components/BudgetSummary';
 import CurrentDateSubtitle from '../../components/subtitles/CurrentDateSubtitle';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const currentBalance = 1420.69;
 const income = 2000.00;
 const expenses = 579.31;
 
 const Home = () => {
+   const { height } = useWindowDimensions();
+
    return (
-      <View>
-         <ScreenHeaderComponent title="Dashboard" />
-         <CurrentDateSubtitle />
-         <BalanceSummaryComponent currentBalance={currentBalance} income={income} expenses={expenses} />
-         <View style={{
-            height: '56%',
-         }}>
-            <BudgetSummary />
+      <SafeAreaView>
+         <View>
+            <ScreenHeaderComponent title="Dashboard" />
+            <CurrentDateSubtitle />
+            <BalanceSummaryComponent currentBalance={currentBalance} income={income} expenses={expenses} />
+            <View style={{
+               height: height * .54,
+            }}>
+               <BudgetSummary />
+            </View>
          </View>
-      </View>
+      </SafeAreaView>
    )
 };
 
