33873cc814f83c453482eb881cacf6bf2c62fd07
MandoMG <mando.mg.91@gmail.com>
2022-07-18 06:20:00 -0500

    Added icon for categories over budget

--- packages/mobile/src/screens/Records/components/RecordsGraphs.tsx
+++ packages/mobile/src/screens/Records/components/RecordsGraphs.tsx
@@ -7,6 +7,8 @@ import commonStyles from '../../../common/CommonStyles';
 import textStyles from '../../../common/TextStyles';
 import { BudgetSummaryItem } from '../../../types';
 import useGraphBar from '../../../hooks/useGraphBar';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import Colors from '../../../common/Colors';
 
 const mockData: BudgetSummaryItem[] = [
   { categoryName: 'Restaurants', categoryValue: 119.52, categoryBudget: 200.00, budgetDifference: 80.48, hasBudget: true, isUnderBudget: true },
@@ -47,6 +49,11 @@ const RecordsGraphs = ({ budgetGraphData }: RecordsGraphsProps) => {
               <View style={commonStyles.flexRow}>
                 <Text style={[commonStyles.flexOne, textStyles.itemText]}>
                   {TextUtil.formatCurrency(item.categoryValue)}
+                  {!item.isUnderBudget && (
+                    <View style={{ paddingLeft: 5 }}>
+                      <Icon name="exclamation-circle" style={{ color: Colors.expenseOrange }} />
+                    </View>
+                  )}
                 </Text>
                 <Text style={textStyles.itemText}> {getCategoryBudgetText(item)} </Text>
               </View>
